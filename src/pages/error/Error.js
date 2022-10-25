a059339c007757e8c6c51999e07360b5832e3d50
islammd39e <islammdrahat308@gmail.comm>
2022-10-25 12:05:39 +0600

    error pages upload

---
+++ src/pages/error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='text-center'>
+            <h3>Sorry This Page (404) Not Found !!</h3>
+        </div>
+    );
+};
+
+export default Error;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Blog from "../pages/blog/Blog";
+import Error from "../pages/error/Error";
 import FAQ from "../pages/faq/FAQ";
 import LogIn from "../pages/login/LogIn";
 import SignUp from "../pages/signup/SignUp";
@@ -31,6 +32,10 @@ export const routes = createBrowserRouter([
             {
                 path:'/signup',
                 element:<SignUp></SignUp>
+            },
+            {
+                path:'*',
+                element:<Error></Error>
             }
         ]
     }
